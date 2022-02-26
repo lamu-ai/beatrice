44a607d5f122b7e675b4c0f7af55d155f64152b7
Greta Renzi <greta@lamu.ai>
2022-02-26 23:40:00 +0100

    Update Anime model and routes

--- beatrice/backend/app/api/v1/anime.py
+++ beatrice/backend/app/api/v1/anime.py
@@ -9,20 +9,31 @@ import sqlmodel
 
 from app.api import dependencies
 from app.crud import anime as anime_crud
-from app.models import anime
-from app.models import patron
+from app.models import anime as anime_model
+from app.models import patron as patron_model
+from app.models import response
 
 router = fastapi.APIRouter()
 
 
-@router.post("/", response_model=anime.AnimeRead, status_code=201)
+@router.post("/",
+             response_model=anime_model.AnimeRead,
+             status_code=201,
+             responses={
+                 401: {
+                     "model": response.Response
+                 },
+                 409: {
+                     "model": response.Response
+                 }
+             })
 def create_anime(
     *,
     session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
-    anime_in: anime.AnimeCreate,
-    current_patron: patron.Patron = fastapi.Depends(  # pylint: disable=unused-argument
+    anime_in: anime_model.AnimeCreate,
+    current_patron: patron_model.Patron = fastapi.Depends(  # pylint: disable=unused-argument
         dependencies.get_current_active_patron),
-) -> anime.Anime:
+) -> anime_model.Anime:
     """Creates a new anime."""
     anime_db = anime_crud.AnimeCRUD.get_by_title(session, anime_in.title_en)
 
@@ -31,56 +42,87 @@ def create_anime(
             status_code=status.HTTP_409_CONFLICT,
             detail="An anime with this title already exists in the system.",
         )
+    if current_patron.id != anime_in.proposed_by:
+        raise fastapi.HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="https://www.youtube.com/watch?v=Z4oDZCJMDeY")
 
-    #if settings.EMAILS_ENABLED and anime_in.email:
-    #    send_new_account_email(email_to=anime_in.email,
-    #                           username=anime_in.email,
-    #                           password=anime_in.password)
+    anime = anime_crud.AnimeCRUD.create(session, model_in=anime_in)
 
-    user = anime_crud.AnimeCRUD.create(session, model_in=anime_in)
-
-    return user
+    return anime
 
 
-@router.get("/{anime_id}", response_model=anime.AnimeReadWithPatron)
+@router.get("/{anime_id}",
+            response_model=anime_model.AnimeReadWithPatron,
+            responses={
+                401: {
+                    "model": response.Response
+                },
+                404: {
+                    "model": response.Response
+                }
+            })
 def read_anime(
     *,
     session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
     anime_id: int,
-    current_patron: patron.Patron = fastapi.Depends(  # pylint: disable=unused-argument
+    current_patron: patron_model.Patron = fastapi.Depends(  # pylint: disable=unused-argument
         dependencies.get_current_active_patron),
-) -> anime.Anime:
-    """Returns a anime given the id."""
-    return anime_crud.AnimeCRUD.read(session, anime_id)
+) -> anime_model.Anime:
+    """Returns an anime given the id."""
+    anime = anime_crud.AnimeCRUD.read(session, anime_id)
+
+    if not anime:
+        raise fastapi.HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                    detail="Anime not found.")
+
+    return anime
 
 
-@router.get("/", response_model=List[anime.AnimeRead])
+@router.get("/",
+            response_model=List[anime_model.AnimeRead],
+            responses={401: {
+                "model": response.Response
+            }})
 def read_anime_list(
-        session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
-        current_patron: patron.Patron = fastapi.Depends(  # pylint: disable=unused-argument
-            dependencies.get_current_active_patron),
-        offset: int = 0,
-        limit: int = fastapi.Query(default=100, le=100),
-) -> List[anime.Anime]:
+    session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
+    current_patron: patron_model.Patron = fastapi.Depends(  # pylint: disable=unused-argument
+        dependencies.get_current_active_patron),
+    offset: int = 0,
+    limit: int = fastapi.Query(default=100, le=100),
+) -> List[anime_model.Anime]:
     """Returns a list of anime."""
     return anime_crud.AnimeCRUD.read_multi(session, offset=offset, limit=limit)
 
 
-@router.put("/", response_model=anime.AnimeRead)
+@router.put("/",
+            response_model=anime_model.AnimeRead,
+            responses={
+                401: {
+                    "model": response.Response
+                },
+                404: {
+                    "model": response.Response
+                }
+            })
 def update_anime(
     *,
     session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
-    current_patron: patron.Patron = fastapi.Depends(  # pylint: disable=unused-argument
+    current_patron: patron_model.Patron = fastapi.Depends(  # pylint: disable=unused-argument
         dependencies.get_current_active_patron),
     anime_id: int,
-    anime_in: anime.AnimeUpdate,
-) -> anime.Anime:
+    anime_in: anime_model.AnimeUpdate,
+) -> anime_model.Anime:
     """Updates a anime."""
     anime_db = anime_crud.AnimeCRUD.read(session, anime_id)
 
     if not anime_db:
         raise fastapi.HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                     detail="Anime not found.")
+    if current_patron.id != anime_db.proposed_by:
+        raise fastapi.HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Cannot update an anime inserted by another patron.")
 
     anime_db = anime_crud.AnimeCRUD.update(session,
                                            model_db=anime_db,
@@ -89,12 +131,21 @@ def update_anime(
     return anime_db
 
 
-@router.delete("/", status_code=204)
+@router.delete("/",
+               status_code=204,
+               responses={
+                   401: {
+                       "model": response.Response
+                   },
+                   404: {
+                       "model": response.Response
+                   }
+               })
 def delete_anime(
     *,
     session: sqlmodel.Session = fastapi.Depends(dependencies.get_session),
     anime_id: int,
-    current_patron: patron.Patron = fastapi.Depends(  # pylint: disable=unused-argument
+    current_patron: patron_model.Patron = fastapi.Depends(  # pylint: disable=unused-argument
         dependencies.get_current_active_superuser),
 ):
     """Deletes an anime."""
--- beatrice/backend/app/models/anime.py
+++ beatrice/backend/app/models/anime.py
@@ -1,18 +1,16 @@
 """Anime models."""
 
-from typing import TYPE_CHECKING
-import datetime
+import typing
 
 import sqlmodel
 
 from app.models import mixins
 
-if TYPE_CHECKING:
+if typing.TYPE_CHECKING:
     from app.models.patron import Patron, PatronRead
 
 
-class AnimeBase(sqlmodel.SQLModel, mixins.TimestampsMixin,
-                mixins.ProposalMixin):
+class AnimeBase(sqlmodel.SQLModel, mixins.ProposalMixin, mixins.LinksMixin):
     """Base Anime model."""
     title_en: str
     title_jp: str | None
@@ -22,7 +20,7 @@ class AnimeBase(sqlmodel.SQLModel, mixins.TimestampsMixin,
     notes: str | None
 
 
-class Anime(AnimeBase, table=True):
+class Anime(AnimeBase, mixins.TimestampsMixin, table=True):
     """Anime database model."""
     id: int | None = sqlmodel.Field(default=None, primary_key=True)
 
@@ -42,13 +40,11 @@ class AnimeReadWithPatron(AnimeRead):
     patron: "PatronRead" = None
 
 
-class AnimeUpdate(sqlmodel.SQLModel):
+class AnimeUpdate(sqlmodel.SQLModel, mixins.LinksMixin):
     """Anime update model."""
     title_en: str | None = None
     title_jp: str | None = None
     season_anime: int | None = None
-    year: int | None = None
+    year: int | None = sqlmodel.Field(default=None, ge=1900)
     season_year: str | None = None
     notes: str | None = None
-    link: str | None = None
-    updated_at: datetime.datetime
