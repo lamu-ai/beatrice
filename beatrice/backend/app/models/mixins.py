4a9a1e311bbaf5eec324360be6acc338a0f790d8
Greta Renzi <greta@lamu.ai>
2022-02-26 11:39:13 +0100

    Add ProposalMixin mixin

--- beatrice/backend/app/models/mixins.py
+++ beatrice/backend/app/models/mixins.py
@@ -3,6 +3,7 @@
 import datetime
 
 import pydantic
+import sqlmodel
 
 
 class TimestampsMixin(pydantic.BaseModel):
@@ -14,3 +15,12 @@ class TimestampsMixin(pydantic.BaseModel):
     """
     created_at: datetime.datetime
     updated_at: datetime.datetime
+
+
+class ProposalMixin(pydantic.BaseModel):
+    """Mixin that defines proposal-related fields.
+
+    Attributes:
+        proposed_by: The id of the patron who proposed the media.
+    """
+    proposed_by: int = sqlmodel.Field(default=None, foreign_key="patron.id")
